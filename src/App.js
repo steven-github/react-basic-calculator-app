c8e41f16e738d609ba46da4aa7abd6372329534e
Steven S <freelancers2010@gmail.com>
2023-09-15 19:49:20 -0600

    uploading files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [input, setInput] = useState("");
+  const [result, setResult] = useState("");
+
+  const handleButtonClick = (value) => {
+    setInput(input + value);
+  };
+
+  const clearInput = () => {
+    setInput("");
+    setResult("");
+  };
+
+  const calculateResult = () => {
+    try {
+      const calculatedResult = eval(input);
+      setResult(calculatedResult);
+    } catch (error) {
+      setResult("Error");
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="min-h-screen flex justify-center items-center bg-gray-100">
+      <div className="bg-white p-4 rounded shadow-lg">
+        <input type="text" className="w-full mb-2 p-2 border rounded" value={input} readOnly />
+        <div className="grid grid-cols-4 gap-2">
+          <button className="col-span-3 bg-gray-200 p-2 rounded" onClick={clearInput}>
+            Clear
+          </button>
+          <button className="bg-red-500 p-2 rounded text-white" onClick={() => handleButtonClick("/")}>
+            /
+          </button>
+          <button onClick={() => handleButtonClick("7")}>7</button>
+          <button onClick={() => handleButtonClick("8")}>8</button>
+          <button onClick={() => handleButtonClick("9")}>9</button>
+          <button className="bg-red-500 p-2 rounded text-white" onClick={() => handleButtonClick("*")}>
+            *
+          </button>
+          <button onClick={() => handleButtonClick("4")}>4</button>
+          <button onClick={() => handleButtonClick("5")}>5</button>
+          <button onClick={() => handleButtonClick("6")}>6</button>
+          <button className="bg-red-500 p-2 rounded text-white" onClick={() => handleButtonClick("-")}>
+            -
+          </button>
+          <button onClick={() => handleButtonClick("1")}>1</button>
+          <button onClick={() => handleButtonClick("2")}>2</button>
+          <button onClick={() => handleButtonClick("3")}>3</button>
+          <button className="bg-red-500 p-2 rounded text-white" onClick={() => handleButtonClick("+")}>
+            +
+          </button>
+          <button className="col-span-2" onClick={() => handleButtonClick("0")}>
+            0
+          </button>
+          <button onClick={calculateResult}>=</button>
+        </div>
+        <p className="mt-2 text-2xl font-bold">{result}</p>
+      </div>
     </div>
   );
 }
